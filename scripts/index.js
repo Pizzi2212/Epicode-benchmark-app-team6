4bc8588a4a5e0750d7b0138c3796ea1bed4d6631
Freskies <43386854+Freskies@users.noreply.github.com>
2024-10-25 10:15:04 +0200

    continue dialog

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,31 @@
 const form = document.getElementById("proceed-form");
 const popup = document.getElementById("popup");
-const settings = document.getElementById("settings");
 const wrapper = document.querySelector(".wrapper");
+const closeButton = document.querySelector(".close");
+const settings = document.getElementById("settings");
+const difficulty = document.getElementById("difficulty");
+const difficultyLabel = document.getElementById("difficulty-label");
+
+const valueToDifficulty = value => {};
+
+popup.addEventListener("click", e => {
+	if (!wrapper.contains(e.target)) popup.close();
+});
+
+closeButton.addEventListener("click", () => {
+	popup.close();
+});
 
 form.addEventListener("submit", e => {
 	e.preventDefault();
 	popup.showModal();
 });
 
-popup.addEventListener("click", e => {
-	if (!wrapper.contains(e.target)) popup.close();
-});
-
 settings.addEventListener("submit", e => {
 	e.preventDefault();
 	location.href = "quiz.html?questions=10&difficulty=easy";
+});
+
+difficulty.addEventListener("change", () => {
+	console.log(difficulty.value);
 });
